cb8e86c864da4929e9cf3d65e741e70f6efb8653
Santiago <Kuja2022@gmail.com>
2023-10-18 13:47:28 +0200

    commit, intento conflictos

--- prueba.py
+++ prueba.py
@@ -1,3 +1,11 @@
 lista = [10,20,30]
 for i in lista:
-    print(i)
+    print(i*9)
+
+lista_2 = [20,40,60]
+for i in lista_2:
+    print(i*8)
+
+lista_3 = [30,60,90]
+for i in lista_3:
+    print(i/2)
