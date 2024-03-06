fc8dde9c316c38cbcd84168be17185247df9c6b7
Santiago <Kuja2022@gmail.com>
2024-03-06 13:36:03 +0100

    push rama segunda

--- prueba_rama.py
+++ prueba_rama.py
@@ -1,3 +1,19 @@
 lista = [20,40,20]
+print("10")
+lista = [10,20,5,28,30]
+print("10")
+print("14342425")
+for i in lista:
+    print(i/2) 
+    print("hohahalahahah")
+
+lista = [1,54,22,30]
+for i in lista:
+    print(i*2)
+    print("hola") 
+
+lista = [16,20,30]
+for i in lista:
+    print(i*2)
 for i in lista:
     print(i)
