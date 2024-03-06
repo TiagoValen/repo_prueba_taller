70f8d1b2a2e1e008eab4c48bf1ed1ded6656de6e
Santiago <Kuja2022@gmail.com>
2024-03-06 13:30:38 +0100

    commit cambios dev

--- prueba_rama.py
+++ prueba_rama.py
@@ -1,3 +1,17 @@
-lista = [20,40,20]
+lista = [230,4440,43420]
+lista = [10,20,30]
 for i in lista:
+   
+    print("10")
+for i in lista:  
+    print((i*2) - 1) 
+    print("hohhahah")
+    
+lista = [14,22,30]  
+for i in lista:  
+    print(i**2) 
+print("hola")
+lista = [16,20,30]  
+for i in lista:  
+    print(i*2) 
     print(i)
